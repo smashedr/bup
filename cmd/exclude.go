4d181639c9166ebfa839c71b9b9acbc78c1310db
Shane <6071159+smashedr@users.noreply.github.com>
2026-01-30 19:55:34 -0800

    CLeanup

--- cmd/exclude.go
+++ cmd/exclude.go
@@ -6,11 +6,11 @@ import (
 	"github.com/spf13/viper"
 )
 
-var infoCmd = &cobra.Command{
-	Use:     "info [name]",
-	Aliases: []string{"i", "in", "inf"},
-	Short:   "Show information about application",
-	Long:    "Show information about application.",
+var excludeCmd = &cobra.Command{
+	Use:     "exclude add/remove [name]",
+	Aliases: []string{"e", "ex", "exc"},
+	Short:   "Show, add or remove excludes",
+	Long:    "Show, add or remove excludes.",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("--------------------\n")
 		fmt.Printf("args: %s\n", args)
@@ -18,11 +18,12 @@ var infoCmd = &cobra.Command{
 		destination := viper.GetString("destination")
 		fmt.Printf("destination: %s\n", destination)
 		fmt.Printf("excludes: %s\n", viper.GetStringSlice("excludes"))
+		fmt.Printf("INOP - this command only list excludes. Update the Config File to edit them.\n")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(infoCmd)
+	rootCmd.AddCommand(excludeCmd)
 
 	// Here you will define your flags and configuration settings.
 
